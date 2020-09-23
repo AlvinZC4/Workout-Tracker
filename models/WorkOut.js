4231db4db2ca06943b8107ea7fe25928d185d9b1
Alvin <alvinzcoxiv@gmail.com>
2020-09-22 22:44:28 -0400

    created scheman for workout tracker

---
+++ models/WorkOut.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const workoutSchema = new Schema ({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Give a name for your exercise.",
+                validate: {
+                    minlength: 1,
+                    maxlength: 50,
+                }
+            },
+            duration: {
+                type: Number,
+                required: true
+            },
+            weight: {
+                type: Number,
+                required: function() {return this.type === "resistance"}
+            },
+            reps: {
+                type: Number,
+                required: function() {return this.type === "resistance"}
+            },
+            sets: {
+                type: Number,
+                required: function() {return this.type === "resistance"}
+            },
+            distance: {
+                type: Number,
+                required: function() {return this.type === "cardio"}
+            }
+        }
+    ]
+})
