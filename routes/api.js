067b19ab9591c65bfd09c236ad45018db6d617ed
Alvin <alvinzcoxiv@gmail.com>
2020-09-28 22:45:28 -0400

    created the add exercise route

--- routes/api.js
+++ routes/api.js
@@ -11,4 +11,18 @@ router.get("/api/workouts", (req, res) => {
     })
 })
 
+router.put("/api/workouts/:id", function(req, res) {
+    const workOut = req.params.id
+     console.log("workout id: ", workOut)
+    const newExercise = req.body
+    WorkOut.findByIdAndUpdate( workOut, {$push: {exercises: newExercise}}).then(dbWorkOut => {
+        console.log("dbWorkout add Exercise: ", dbWorkOut)
+        res.json(dbWorkOut)
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    })
+
+})
+
 module.exports = router
